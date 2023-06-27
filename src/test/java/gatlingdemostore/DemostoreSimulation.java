bd6b24b1eed230e029c577c90d1a4a1d474456a6
Jérémy Flusin <jflusin@gatling.io>
2023-06-27 17:07:06 +0200

    chore: some typos + static scenario + pom.xml placeholders

--- src/test/java/gatlingdemostore/DemostoreSimulation.java
+++ src/test/java/gatlingdemostore/DemostoreSimulation.java
@@ -13,7 +13,7 @@ public class DemostoreSimulation extends Simulation {
     private static final String DOMAIN = "demostore.gatling.io";
     private static final HttpProtocolBuilder HTTP_PROTOCOL = http.baseUrl("http://" + DOMAIN);
 
-    private static final ScenarioBuilder scn =
+    private final ScenarioBuilder scn =
             scenario("Browse glasses")
                     .exec(
                             http("Load Home Page")
